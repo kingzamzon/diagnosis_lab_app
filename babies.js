63704b8a13b25cca7ade72c980b1aa9805a5a43f
Ade Niyi <adesanoye.samson@yahoo.com>
2019-03-22 14:18:04 -0700

    created and mofified javascript file

---
+++ babies.js
@@ -0,0 +1,63 @@
+ function baby1() {
+          var sh = document.getElementById("imgcon");
+            if (sh.style.display === "none") {
+                 sh.style.display = "block";
+				 document.getElementById("imgcon").src="baby/b1.png";
+            } else {
+                sh.style.display = "none";
+            }
+        }
+function baby2() {
+          var sh = document.getElementById("imgcon");
+            if (sh.style.display === "none") {
+                 sh.style.display = "block";
+				 document.getElementById("imgcon").src="baby/b2.png";
+            } else {
+                sh.style.display = "none";
+				
+            }
+        }
+function baby3() {
+          var sh = document.getElementById("imgcon");
+            if (sh.style.display === "none") {
+                 sh.style.display = "block";
+            } else {
+                sh.style.display = "none";
+				document.getElementById("imgcon").src="baby/b3.png";
+            }
+        }
+function baby4() {
+          var sh = document.getElementById("imgcon");
+            if (sh.style.display === "none") {
+                 sh.style.display = "block";
+            } else {
+                sh.style.display = "none";
+				document.getElementById("imgcon").src="baby/b4.png";
+            }
+        }
+function baby5() {
+          var sh = document.getElementById("imgcon");
+            if (sh.style.display === "none") {
+                 sh.style.display = "block";
+            } else {
+                sh.style.display = "none";
+				document.getElementById("imgcon").src="baby/b5.png";
+            }
+        }
+function baby6() {
+          var sh = document.getElementById("imgcon");
+            if (sh.style.display === "none") {
+                 sh.style.display = "block";
+            } else {
+                sh.style.display = "none";
+				document.getElementById("imgcon").src="baby/b6.png";
+            }
+        }
+function removeb() {
+          var sh = document.getElementById("buttons");
+            if (sh.style.display === "none") {
+                 sh.style.display = "block";
+            } else {
+                sh.style.display = "none";
+            }
+        }
